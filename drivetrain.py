c633ccf370dbd5bd3078dc828a7d1e7f527e5f1d
Emiliano Garcia <A00344591@itesm.mx>
2018-01-15 18:31:14 -0600

    Agregar mis funciones

--- drivetrain.py
+++ drivetrain.py
@@ -1,8 +1,7 @@
-import wpilib
 import math
 
 class DriveTrain:
-    def __init__(self, left_motor: wpilib.PWMSpeedController, right_motor: wpilib.PWMSpeedController):
+    def __init__(self, left_motor, right_motor):
         self.left_motor = left_motor
         self.right_motor = right_motor
 
@@ -19,67 +18,52 @@ class DriveTrain:
         pass
 
     def getMotorDerDpad(self, pad, gatillo):
-        pass
+        if pad == 0:
+            return -1 * gatillo
+        elif pad == 45:
+            return 0 * gatillo
+        elif pad == 90:
+            return 1 * gatillo
+        elif pad == 135:
+            return 0 * gatillo
+        elif pad == 180:
+            return 1 * gatillo
+        elif pad == 225:
+            return 1 * gatillo
+        elif pad == 270:
+            return -1 * gatillo
+        elif pad == 315:
+            return -1 * gatillo
+        else:
+            return 0
 
     def toDegrees(self, angrad):
-        pass
+        degrees = math.toDegrees(angrad)
+        if degrees < 0:
+            return -degrees
+        else:
+            return 360 - degrees
 
     def stop(self):
-        pass
+        set_motors(0,0)
 
-    def drive(self, stick: wpilib.Joystick):
-        pass
-
-    def set_motors(self, left_power, right_power):
-        self.left_motor.set(left_power)
-        self.right_motor.set(right_power)
-
-    def drive_with_joystick(self, stick: wpilib.Joystick):
-        trigger = self.get_trigger(stick)
-        x       = stick.getRawAxis(0)
-        y       = stick.getRawAxis(1)
-
-        dead_zone = 0.15
-
-        if math.fabs(x) < dead_zone and math.fabs(y) < dead_zone:
-            trigger = 0
-            x = 0
-            y = 0
-
-        radians = math.atan2(y, x)
-        heading = self.to_degrees(radians)
-
-        self.drive_with_heading(heading, trigger)
-
-    def drive_with_pad(self, stick: wpilib.Joystick):
-        trigger = self.get_trigger(stick)
-        dpad    = stick.getPOV()
-
-        left_power  = get_left_motor(dpad, trigger)
-        right_motor = get_right_motor(dpad, trigger)
-
-        self.set_motors(left_power, right_motor)
-
-    def drive_with_heading(self, heading, trigger):
-        left_power  = get_left_motor_dpad(heading, trigger)
-        right_motor = get_right_motor_dpad(heading, trigger)
-
-        self.set_motors(left_power, right_motor)
-
-    def get_trigger(self, stick: wpilib.Joystick):
-        first_trigger  = stick.getRawAxis(3)
-        second_trigger = stick.getRawAxis(2)
-
-        stateA = stick.getRawButton(1)
-        stateX = stick.getRawButton(3)
-        stateY = stick.getRawButton(4)
-
-        if stateA:
-            return 0.30
-        elif stateX:
-            return 0.50
-        elif stateY:
-            return 0.70
+    def drive(self, stick):
+        if stick.getPOV() != -1:
+            driveDpad(stick)
         else:
-            return (first_trigger * 0.5) + (second_trigger * 0.5)
+            driveStick(stick)
 
+    def set_motors(self, potencia_izq, potencia_der):
+        pass
+
+    def driveWithJoystick(self, stick):
+        pass
+
+    def driveWithDpad(self, stick):
+        pass
+
+    def driveWithHeading(self, heading, gatillo):
+        pass
+
+    def getGatillo(self, stick):
+        pass
