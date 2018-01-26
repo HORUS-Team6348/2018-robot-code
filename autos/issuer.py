59b7cc43e549c98a620e933742369cc70a2e3562
Emilio Mendoza <emilio2601@gmail.com>
2018-01-25 19:48:53 -0600

    Mira, si está feo, pero jala, no está feo...
    No hagan esto en casa chavos

---
+++ autos/issuer.py
@@ -0,0 +1,37 @@
+import autos.center_left as cl
+import autos.center_right as cr
+import autos.cross as crss
+import autos.left_scale as lscale
+import autos.left_switch as lswitch
+import autos.right_scale as rscale
+import autos.right_switch as rswitch
+
+def center_left(delay):
+    pass
+    #return cl.CenterLeft(delay)
+
+def center_right(delay):
+    pass
+    #return cr.CenterRight(delay)
+
+def cross(delay):
+    pass
+    #return crss.Cross(delay)
+
+def left_scale():
+    pass
+    #return lscale.LeftScale()
+
+def left_switch():
+    pass
+    #return lswitch.LeftSwitch()
+
+def right_scale():
+    pass
+    #return rscale.RightScale()
+
+def right_switch():
+    pass
+    #return rswitch.RightSwitch()
+
+
